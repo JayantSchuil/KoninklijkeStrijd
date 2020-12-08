57a05b2ed0ed8b2904dc7659e6d79dbaa8f202bd
InnocentNyuu <sharoek@live.nl>
2020-12-08 10:12:44 +0100

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -8,24 +8,19 @@ def setup():
     
 def draw():
     rectover = True if mouseX > 315 and mouseX < 315 + 150 and mouseY > 300 and mouseY < 300 + 55 else False 
-
     if rectover == True:
         fill(51)
     else: 
         fill(255)
     rec(315,300,150,55)
-    if mouseX > 315 and mouseX < 315 + 150 and mouseY > 300 and mouseY < 300 + 55:
-        recglow(315,300,150,55)
-    texts('Credit', 350, 335)
-    rectangle()
     rec(315,225,150,55)
-    if mouseX > 315 and mouseX < 315 + 150 and mouseY > 225 and mouseY < 225 + 55:
-        recglow(315,225,150,55)
+    texts('Credit', 350, 335)
     texts('FIGHT', 350, 260)
         
 def rectangle():
     pg.beginDraw()
     pg.background(255)
+    pg.filter(BLUR, 3)
     pg.stroke(0)
     pg.endDraw()
     image(pg, 200, 50)
@@ -34,6 +29,8 @@ def rec(w,h,sizew,sizeh):
     global color_rec
     fill(color_rec)
     rect(w,h,sizew,sizeh)
+    if mouseX > w and mouseX < w + sizew and mouseY > h and mouseY < h + sizeh:
+        recglow(w,h,sizew,sizeh)
    
 
 def recglow(w,h,sizew,sizeh):
