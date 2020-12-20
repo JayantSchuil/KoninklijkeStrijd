b0bfd6122dbd741a26a8e4625bd21b875d41534d
InnocentNyuu <sharoek@live.nl>
2020-12-20 12:45:55 +0100

    *UPDATED*
    ALLES WERKT , ALLEEN 1 ERROR NA 2 KEER UITVOEREN VAN PROGRAMMA

--- Menu.py
+++ Menu.py
@@ -1,37 +1,48 @@
 SCREENSTATE = 0 
+bgIndex = 0
+x = -100
 
 def setup():
     size(800, 600)
-    global bg, SCREENSTATE
-
-
+    global bg, SCREENSTATE, bgImages
+    frameRate(25)
+    bgImages = [loadImage(str(i).zfill(3) + ".jpg") for i in range(10, 191)]
+        
 def draw():
+    global bgIndex
+    background(bgImages[bgIndex%181])
+    bgIndex += 1
     if (SCREENSTATE == 0):
         drawMenu()
     if (SCREENSTATE == 2):
         drawCredits() 
+    if (SCREENSTATE == 3):
+        exit()
                             
 def drawMenu(): 
-    global SCREENSTATE 
-    bg = loadImage('Castle1.gif')
-    background(0)
-    image(bg, 0, 0)
+    global SCREENSTATE, bgIndex
     texts('Battle', 675, 325)
     texts('Credits', 675, 375)
+    texts('Quit Game', 675, 425)
     noStroke()
     fill(0,50)
     rect(600, 300, 150, 200)
     mouseOver()
     
 def drawCredits():
-    global SCREENSTATE     
-    bg = loadImage('Castle1.gif')
-    background(0)
-    image(bg, 0, 0)
+    global SCREENSTATE, bgIndex,x   
     texts('Return', 675, 325)
     noStroke()
     fill(0,50)
-    rect(600, 300, 150, 200) 
+    rect(600, 300, 150, 200)
+    if x == 620:
+        x = -100 
+    texts('Gemaakt door:', 400 , x)
+    texts('Sharoek', 400 , x + 25)
+    texts('Jayant:', 400 , x + 50)
+    texts('Dennis:', 400 , x + 75)
+    texts('Faraaz:', 400 , x + 100)
+    x += 5   
     if mouseX <= 703 and mouseX >= 646 and mouseY <= 323 and mouseY >= 309:
         texts('Return', 675, 325)
         fill(255)
@@ -41,11 +52,14 @@ def drawCredits():
    
 def texts(x, pos1, pos2):
     fill('#c4d6e2')
-    textSize(23)
+    textSize(20)
     textAlign(CENTER)
     text(x, pos1, pos2)
   
 def mouseOver():
+    if mouseX <= 725 and mouseX >= 625 and mouseY <= 423 and mouseY >= 411:
+        texts('Quit Game', 675, 425)
+        fill(255)
     if mouseX <= 703 and mouseX >= 646 and mouseY <= 323 and mouseY >= 309:
         texts('Battle', 675, 325)
         fill(255)
@@ -64,6 +78,9 @@ def mouseClicked():
         SCREENSTATE = 0
     if SCREENSTATE == 2 and mouseX >= 646 and mouseY <= 323 and mouseY >= 309: #Return
         SCREENSTATE = 0
+    if SCREENSTATE == 0 and mouseX >= 625 and mouseY <= 423 and mouseY >= 411: #Quit Game 
+        SCREENSTATE = 3
+    
 
         
         
