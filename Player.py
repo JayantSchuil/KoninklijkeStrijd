1d31cf08fad315df51bce4ca45ba231532efc7b3
InnocentNyuu <sharoek@live.nl>
2021-01-13 18:11:35 +0100

    Met hartjes

--- Player.py
+++ Player.py
@@ -2,15 +2,18 @@ class Player:
     def __init__(self, name, health, colour):
         self.name = name
         self.health = health
-        self.colour = colour
+        self.heartImageBlue = loadImage("heart_blue" + ".png")
+        self.heartImageGreen = loadImage("heart_green" + ".png") 
+        self.heartImageRed = loadImage("heart_red" + ".png")
+        self.heartImageYellow = loadImage("heart_yellow" + ".png")
 
     def show(self, x, y):
         fill(255)
         text(self.name, x, y)
         for i in range(self.health):
             #Place the image of the heart with on the right place.
-            # image(self.heartImage, x + 17 * i, y + 15, 15, 15)
-            text("X", x + 20 * i, y + 20)
+            image(self.heartImageBlue, x + 17 * i, y + 15, 15, 15)
+            #text("X", x + 20 * i, y + 20)
 
 
         
